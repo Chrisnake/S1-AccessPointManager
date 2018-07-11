71a9d312cc33deae4a84471bdb76feb0578d2be8
Christian <1605942@brunel.ac.uk>
2018-07-11 23:15:13 +0100

    Implemented JavaSwing GUI start up page and register page. Inclusion of combobox fields.

--- UAPManager/src/Register.java
+++ UAPManager/src/Register.java
@@ -1,17 +1,40 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JButton;
 
 public class Register extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
+	private JTextField userPassword;
+	private JTextField userID;
+	private JLabel lblUniversityId;
+	private JLabel lblNewLabel;
+	private JLabel lblPosition;
+	private JLabel lblAccomodation;
+	private JLabel lblDepartment;
+ 
+	/*
+	 * String arrays containing information for combobox fields
 	 */
+	
+	String[] arrayuserType = new String[] {"Student", "Lecturer", "Cleaner"};
+	
+	String[] arrayDepartment = new String[] {"Engineering", "Biomedical Sciences", "Business/Accounting", "Communications/Media Studies", 
+											 "Computer Science", "English", "Economics/Finance", "Education", "Games Design", "Law", 
+											 "Mathematics", "Music", "Psyiotheraphy", "Sports/Health Science", "Arts/Theatre"};
+	
+	String[] arrayAccomodation = new String[] {"Bishop Complex", "Faraday Hall", "Fleming Hall", "Gailbraith Hall", "Isambard Complex",
+											   "Mill Hall", "Lancaster Complex", "Chepstow Hall", "Clifton Hall","Saltash Hall"};
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -25,16 +48,98 @@ public class Register extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Register() {
+	public Register() 
+	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		/*
+		 * JPanel componenets
+		 */
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 153, 153));
+		panel.setBounds(0, 0, 208, 278);
+		contentPane.add(panel);
+		
+		/*
+		 *  JTextField componenets
+		 */
+		
+		userPassword = new JTextField();
+		userPassword.setBounds(302, 101, 130, 26);
+		contentPane.add(userPassword);
+		userPassword.setColumns(10);
+		
+		userID = new JTextField();
+		userID.setBounds(302, 76, 130, 26);
+		contentPane.add(userID);
+		userID.setColumns(10);
+		
+		/*
+		 * JComboBox components
+		 */
+		
+		JComboBox<String> userDepartment = new JComboBox<>(arrayDepartment);
+		String selectedDepartment = (String) userDepartment.getSelectedItem();
+		userDepartment.setBounds(302, 180, 130, 27);
+		contentPane.add(userDepartment);
+		
+		JComboBox<String> userAccomodation = new JComboBox<>(arrayAccomodation);
+		String selectedAccomodation = (String) userAccomodation.getSelectedItem();
+		userAccomodation.setBounds(302, 154, 130, 27);
+		contentPane.add(userAccomodation);
+		
+		JComboBox <String> userType = new JComboBox<>(arrayuserType);
+		String selectedType= (String) userType.getSelectedItem();
+		userType.setBounds(302, 127, 130, 27);
+		contentPane.add(userType);
+		
+		/*
+		 * JLabel componenets
+		 */
+		
+		lblUniversityId = new JLabel("University ID");
+		lblUniversityId.setBounds(220, 81, 70, 16);
+		lblUniversityId.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(lblUniversityId);
+		
+		lblNewLabel = new JLabel("Password");
+		lblNewLabel.setBounds(220, 106, 75, 16);
+		lblNewLabel.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(lblNewLabel);
+		
+		lblPosition = new JLabel("Position");
+		lblPosition.setBounds(220, 131, 61, 16);
+		lblPosition.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(lblPosition);
+		
+		lblAccomodation = new JLabel("Accomodation");
+		lblAccomodation.setBounds(220, 158, 89, 16);
+		lblAccomodation.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(lblAccomodation);
+		
+		lblDepartment = new JLabel("Department");
+		lblDepartment.setBounds(220, 184, 89, 16);
+		lblDepartment.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(lblDepartment);
+		
+		/*
+		 * JButton components
+		 */
+		
+		JButton backButton = new JButton("Next");
+		backButton.setBounds(371, 231, 61, 29);
+		backButton.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(backButton);
+		
+		JButton nextButton = new JButton("Back");
+		nextButton.setBounds(229, 231, 61, 29);
+		nextButton.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		contentPane.add(nextButton);
 	}
-
 }
--- UAPManager/src/StartUp.java
+++ UAPManager/src/StartUp.java
@@ -1,14 +1,28 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class StartUp {
 
 	private JFrame frame;
+	private JTextField usernameField;
+	private JTextField passwordField;
 
 	/**
-	 * Launch the application.
+	 * @param frame: the frame for the class
+	 * @param usernameField: the username user input
+	 * @param passwordFied: the password user input
 	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -22,20 +36,88 @@ public class StartUp {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
-	public StartUp() {
+	public StartUp() 
+	{
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
+	private void initialize() 
+	{	
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		/**
+		 * JPanel componenets
+		 */
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(0, 153, 153));
+		panel.setBounds(0, 0, 208, 278);
+		frame.getContentPane().add(panel);
+		
+		/**
+		 * JLabel components
+		 */
+		
+		JLabel UserIDText = new JLabel("UniversityID");
+		UserIDText.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		UserIDText.setBounds(220, 126, 75, 16);
+		frame.getContentPane().add(UserIDText);
+		
+		JLabel lblPassword = new JLabel("Password");
+		lblPassword.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 13));
+		lblPassword.setBounds(234, 154, 61, 16);
+		frame.getContentPane().add(lblPassword);
+		
+		JLabel lblUniversityAccessPoint = new JLabel("University Access Point Manager");
+		lblUniversityAccessPoint.setFont(new Font("Apple SD Gothic Neo", Font.BOLD, 14));
+		lblUniversityAccessPoint.setBounds(224, 41, 206, 16);
+		frame.getContentPane().add(lblUniversityAccessPoint);
+		
+		/**
+		 * JTextField components
+		 */
+		
+		usernameField = new JTextField();
+		usernameField.setBounds(300, 120, 130, 26);
+		frame.getContentPane().add(usernameField);
+		usernameField.setColumns(10);
+		
+		passwordField = new JTextField();
+		passwordField.setBounds(300, 148, 130, 26);
+		frame.getContentPane().add(passwordField);
+		passwordField.setColumns(10);
+		
+		/**
+		 * JButton components
+		 */
+		
+		JButton btnLogIn = new JButton("Log In");
+		btnLogIn.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 10));
+		btnLogIn.setBounds(300, 186, 66, 29);
+		frame.getContentPane().add(btnLogIn);
+		btnLogIn.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				
+			}
+		});
+		
+		JButton btnsignUp = new JButton("Sign Up");
+		btnsignUp.setFont(new Font("Apple SD Gothic Neo", Font.PLAIN, 10));
+		btnsignUp.setBounds(364, 186, 66, 29);
+		frame.getContentPane().add(btnsignUp);
+		btnsignUp.addActionListener(new ActionListener() 
+		{
+			public void actionPerformed(ActionEvent e) 
+			{
+				Register newUser = new Register(); //Open the register class frame.
+				newUser.setVisible(true);
+				frame.dispose(); 
+			}
+		});
 	}
-
 }
